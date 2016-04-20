6701a8c517a7b1c469366d641fdeae039ac68ad0
Michael Ieti <m.ieti001@gmail.com>
2016-04-21 01:13:35 +1200

    Initial commit, just finished the spark tutorial for practice letter.

---
+++ src/main/java/App.java
@@ -0,0 +1,48 @@
+import static spark.Spark.*;
+
+public class App {
+	public static void main(String[] args){
+		
+		// This tells Spark to use the 'public' folder as the root location
+		// for all static files. Access using src='/*' where * is a path to
+		// an image or some other static file.
+		staticFileLocation("/public");
+		
+		get("/", (request, response) -> "<!DOCTYPE html>" +
+         "<html>" +
+         "<head>" +
+           "<title>Hello Friend!</title>" +
+           "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+         "</head>" +
+        "<body>" +
+           "<h1>Hello From Afar</h1>" +
+           "<p>Dear Friend,</p>" +
+           "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
+           "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
+           "<p>But I like programming a lot, so I've got that going for me. </p>" +
+           "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
+           "<p>Cheers,</p>" +
+           "<p>Travel Enthusiast Jane</p>" +
+         "</body>" +
+       "</html>"
+	   );
+	   
+	   get("/favorite_photos", (request, response) ->
+      "<!DOCTYPE html>" +
+        "<html>" +
+        "<head>" +
+          "<title>Hello Friend!</title>" +
+          "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+        "</head>" +
+        "<body>" +
+         "<h1>Favorite Traveling Photos</h1>" +
+            "<ul>" +
+              "<li><img src='/images/spin.jpg' alt='Pretty lights.'/></li>" +
+              "<li><img src='/images/apartment.jpg' alt='Peek into apartments.'/></li>" +
+              "<li><img src='/images/jellyfish.jpg' alt='A photo of a jellyfish.'/></li>" +
+            "</ul>" +
+        "</body>" +
+        "</html>"
+		);
+	}
+}
